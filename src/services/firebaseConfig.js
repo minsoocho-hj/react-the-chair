bd17c1b9c4077bd2e64d972492b038f18b60eed5
minsoo cho <khg04170@gmail.com>
2022-05-13 14:50:55 +0100

    🔧 set firebase config and added .env file

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE,
+	messagingSenderId: process.env.REACT_APP_MESSAGE,
+	appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
