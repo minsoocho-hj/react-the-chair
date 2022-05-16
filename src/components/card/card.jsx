5066c2aa2f6f047656bdf98ba74dce22da7f93a0
minsoo cho <khg04170@gmail.com>
2022-05-16 13:22:27 +0100

    app without realtime database done

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import styles from './card.module.css';
 const Card = ({ card, onDelete }) => {
 	return (
--- src/components/cardlist/cardlist.jsx
+++ src/components/cardlist/cardlist.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Card from '../card/card';
 import styles from './cardlist.module.css';
 
 const CardList = ({ cards, onDelete }) => {
-	useEffect(() => {});
+	const cardsRef = useRef();
+
+	const focusToCurrentCard = () => {
+		if (cardsRef.current.lastChild === null) {
+			return;
+		} else {
+			const lastChild = cardsRef.current.lastChild;
+			lastChild?.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
+
+	useEffect(() => {
+		focusToCurrentCard();
+	}, [cards]);
+
 	return (
-		<div className={styles.cards}>
+		<div className={styles.cards} ref={cardsRef}>
 			{Object.keys(cards).length > 0 ? (
 				Object.keys(cards).map((key) => (
 					<Card card={cards[key]} key={key} onDelete={onDelete} />
